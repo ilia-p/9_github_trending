a844516b985fcca454da53f3ff2089c216f107a7
ILIA PETROV <ilia-p@yandex.ru>
2017-07-16 15:48:18 +0300

    Модификация по комментариям от 14072017 от Ильи Лебедева

--- github_trending.py
+++ github_trending.py
@@ -3,21 +3,14 @@ from datetime import date, timedelta
 
 def get_trending_repositories(top_size):
     date_now = date.today()
-    delta_week_ago = timedelta(days=7)
+    delta_week_ago = timedelta(days=days_period)
     date_week_ago = date_now - delta_week_ago
-    url = ('https://api.github.com/search/repositories?q=language:python+created:{}..{}&sort=stars&order=desc').format(date_week_ago, date_now)
-    result = requests.get(url)
+    query = ('created:{}..{}').format(date_week_ago, date_now)
+    search_rules = {'page': '1', 'per_page': str(top_size), 'q': query, 'sort': 'stars', 'order': 'desc'}
+    url = 'https://api.github.com/search/repositories'
+    result = requests.get(url, params = search_rules)
     repo_list_general = result.json()['items']
-    repo_count = 0
-    repo_list  = []
-    for repo in repo_list_general:
-        repo_count += 1
-        if repo_count <= top_size:
-            repo_star = str(repo['stargazers_count'])
-            repo_url  = repo['owner']['repos_url']
-            repo_list.append((repo_star, repo_url))
-        else:
-            break
+    repo_list = [(str(repo['stargazers_count']), repo['owner']['repos_url']) for repo in repo_list_general]        
     return repo_list
     
 def print_trending_repositories(repo_list):
@@ -26,5 +19,6 @@ def print_trending_repositories(repo_list):
 
 if __name__ == '__main__':
     top_size = 20
+    days_period = int(input('Ведите период (кол-во дней): '))
     repo_list = get_trending_repositories(top_size)
     print_trending_repositories(repo_list)
